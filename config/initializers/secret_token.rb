9759ae47aa1b79ab84cd40626cc8f2320aa391d5
Christine <christine@johnsonwebapps.com>
2011-04-14 09:35:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Johnsonwebapps::Application.config.secret_token = 'eab3a9fb25340263d81ebf5ec9669e03646b7e5658ccaaa0386c606a55fb3b00d8950b5d5c2398bd116c71209da340734b7f69fc38bfff682bf1757afe76c774'
